c31926f1c224d0d88fa354961c86ae0283c7dd29
Gerardo Alderete Flores <ge_call@hotmail.com>
2024-08-13 15:50:03 -0600

    docs add

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-HCXWWAQZ.mjs";import"./chunk-24VTMME3.mjs";import"./chunk-6R6UX6AH.mjs";import"./chunk-FOYE6M4L.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
